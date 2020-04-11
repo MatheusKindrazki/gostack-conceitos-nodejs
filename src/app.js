5c064ed07a7fbb3ba26b3c4a194175c78ba77248
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-04-10 21:46:21 -0300

    finalizando desafio

--- src/app.js
+++ src/app.js
@@ -1,33 +1,64 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function getRepoIndex(id) {
+  return repositories.findIndex((repo) => repo.id === id);
+}
+
+function validateRepositoryId(request, response, next) {
+  const repoIndex = getRepoIndex(request.params.id);
+
+  if (repoIndex < 0)
+    return response.status(400).json({ error: "Repository not found" });
+
+  return next();
+}
+
+app.use("/repositories/:id", validateRepositoryId);
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repo = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repo);
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repoIndex = getRepoIndex(request.params.id);
+
+  const repo = repositories[repoIndex];
+  repositories[repoIndex] = {
+    id: repo.id,
+    title,
+    url,
+    techs: techs ? techs : repo.techs,
+    likes: repo.likes,
+  };
+  return response.json(repositories[repoIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = getRepoIndex(request.params.id);
+  repositories.splice(repoIndex, 1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repoIndex = getRepoIndex(request.params.id);
+  repositories[repoIndex].likes++;
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
